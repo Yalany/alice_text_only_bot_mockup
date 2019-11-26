b5bc5cca5b8a0e9cb6d8d97f486a1a9af014fe6e
yalany <nicky_evil@mail.ru>
2019-11-27 00:39:41 +0300

    some config extraction

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -6,11 +6,12 @@ import java.net.InetSocketAddress;
 public final class Launcher {
     private final static int PORT = 8000;
     private final static int BACKLOG = 0;
+    private final static String CONTEXT_PATH = "/game";
 
     public static void main(final String[] args) throws IOException {
         var server = HttpServer.create(new InetSocketAddress(PORT), BACKLOG);
         var bot = new Bot();
-        server.createContext("/game", httpExchange -> {
+        server.createContext(CONTEXT_PATH, httpExchange -> {
             var response = bot.ask(new String(httpExchange.getRequestBody().readAllBytes()));
             httpExchange.sendResponseHeaders(200, response.getBytes().length);
             var responseBody = httpExchange.getResponseBody();
