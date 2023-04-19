eb68afd25df461729ff1dea3eaa7a1ab7a937ac8
Nikita Dobrinskiy <Nikita_Dobrinskiy@epam.com>
2023-04-19 05:25:26 +0300

    Rename Settings to Config

---
+++ src/main/java/game/Config.java
@@ -0,0 +1,7 @@
+package game;
+
+class Config {
+  final static long USER_DATA_CACHE_TIMEOUT_SECONDS = 600;
+  final static String USER_DATA_DIRECTORY = "../user_data/";
+  final static String JSON_POSTFIX = ".json";
+}
--- src/main/java/game/Settings.java
+++
@@ -1,7 +0,0 @@
-package game;
-
-class Settings {
-    final static long USER_DATA_CACHE_TIMEOUT_SECONDS = 600;
-    final static String USER_DATA_DIRECTORY = "../user_data/";
-    final static String JSON_POSTFIX = ".json";
-}
